cb79357b46f0b1a97699d5e9c4b0482fa2f21eaf
VILA ALPERI Marc <marc.vila-alperi@akkodis.com>
2023-09-24 18:42:31 +0200

    added transfer fucntions calculation for lateral and longitudinal channels

--- src/models/plane_model.py
+++ src/models/plane_model.py
@@ -37,7 +37,7 @@ class plane_model:
         # -- Model [float] --
         self.model = model
 
-        # -- Flight condition [struct] -- ESTO ALOMEJOR NO HACE FALTA
+        # -- Flight condition [struct] -- 
         self.FC = FC
 
         # -- Channels --
@@ -68,11 +68,21 @@ class plane_model:
         #     G """
 
     def loadplane(self,model = None):
-
+        """
+        Load flight condition and derivatives into class.
+        Includes non-dimensional characteristics.
+        
+        Parameters:
+            model (optional): The model selfect containing the flight condition and derivatives.
+        
+        Returns:
+            None
+        """
         # -- Load flight condition and derivatives into class --
         # Includes non dimensional characteristics
 
-        scales = 
+        scales = get_scales(model)
+
         # - Geometric and mass properties -
         self.model_name    = model.name
         self.Sw            = model.Sw
@@ -83,6 +93,25 @@ class plane_model:
         self.Iyb           = model.Iyb
         self.Izb           = model.Izb
         self.Ixzb          = model.Ixzb
+
+        # - Non dimensional properties -
+        self.lat,self.lon = {}
+
+        self.lon['t_lon'] = scales['lon']['time']
+        self.lat['t_lat'] = scales['lat']['time']
+        self.lon['mu_lon'] = model.m/scales['lon']['mass']
+        self.lat['mu_lat'] = model.m/scales['lat']['mass']
+        self.lat['Ixb_nd'] = model.Ixb/scales['lat']['inertia']
+        self.lat['Izb_nd'] = model.Izb/scales['lat']['inertia']
+        self.lon['Iyb_nd'] = model.Iyb/scales['lon']['inertia']
+        self.lat['Ixzb_nd'] = model.Ixzb/scales['lat']['inertia']
+
+        # - Flight condition -
+        self.FC = model.FC
+
+        self.FC['CXs'] = model.FC['CTs']*np.cos(model.FC['epss']) + model.FC['CLs']*model.FC['alphabs'] - model.FC['CDs']
+        self.FC['CZs'] = -model.FC['CTs']*np.sin(model.FC['epss']) - model.FC['CLs'] - model.FC['CDs']*model.FC['alphabs']
+        self.FC['Cms'] = 0
 
         # - Performance properties -
         self.Vmax = model.Vmax
@@ -101,11 +130,77 @@ class plane_model:
         self.model['Cl'] = model.Cl
         self.model['Cm'] = model.Cm
         self.model['Cn'] = model.Cn
+
+        # Longitudinal
+        self.lon['CX']['u'] = self.model['CT']['u']*np.cos(self.FC['epss']) - self.model['CD']['u']
+        self.lon['CX']['alpha'] = self.model['CT']['alpha']*np.cos(self.FC['epss']) + self.FC['CLs'] - self.model['CD']['alpha']
+        self.lon['CX']['delta'] = -self.model['CD']['delta']
+        self.lon['CX']['T'] = self.FC['CTs']*np.cos(self.FC['epss'])
+        self.lon['CZ']['u'] = -self.model['CT']['u']*np.sin(self.FC['epss']) - self.model['CL']['u']
+        self.lon['CZ']['alpha'] = -self.model['CT']['alpha']*np.sin(self.FC['epss']) -self.model['CL']['alpha'] - self.FC['CDs']
+        self.lon['CZ']['alphadot'] = -self.model['CL']['alpha']
+        self.lon['CZ']['q'] = -self.model['CL']['q']
+        self.lon['CZ']['delta'] = -self.model['CL']['delta']
+        self.lon['CZ']['T'] = -self.FC['CTs']*np.sin(self.FC['epss'])
+        self.lon['Cm'] = self.model['Cm']
+        self.lon['Cm']['u'] = self.model['Cm']['u'] + self.model['Cm']['Tu']
+        
+        # Lateral-directional
+        self.lat['CY'] = model.CY
+        self.lat['Cl'] = model.Cl
+        self.lat['Cn'] = model.Cn
+        
+        # - Transfer functions -
+        # Longitudinal
+        self.lon['G'] = lon_dynamics(self)
+        
+        # Lateral
+        self.lat['G'] = lat_dynamics(self)
+
     
-    def loadDynamics(self):
+    def set_FC(self,newFC):
+        """
+        Sets a new flight condition of the aircraft.
 
-        # -- State matrix --
-        Alon = np.zeros((4,4))
-        Alat = np.zeros((4,4))
+        Parameters:
+        - newFC: dictionary containing the new flight condition values
+            - 'hs': flight altitude
+            - 'us': cruise speed
+            - 'CTs': static thrust coefficient value
+            - 'epss': static thrust offset angle
+            - 'CLs': static lift coefficient value
+            - 'CDs': static drag coefficient value
+            - 'Cms': static momentum coefficient value
+            - 'CmTs: static thrust engine momentum coefficient value
+            - 'alphabs': static angle of attack 
 
+        Returns:
+        None
+        """
+        # - Flight condition -
+        self.FC = newFC
 
+        self.FC['CXs'] = newFC['CTs']*np.cos(newFC['epss']) + newFC['CLs']*newFC['alphabs'] - newFC['CDs']
+        self.FC['CZs'] = -newFC['CTs']*np.sin(newFC['epss']) - newFC['CLs'] - newFC['CDs']*newFC['alphabs']
+        self.FC['Cms'] = 0
+
+        # Longitudinal
+        self.lon['CX']['u'] = self.model['CT']['u']*np.cos(self.FC['epss']) - self.model['CD']['u']
+        self.lon['CX']['alpha'] = self.model['CT']['alpha']*np.cos(self.FC['epss']) + self.FC['CLs'] - self.model['CD']['alpha']
+        self.lon['CX']['delta'] = -self.model['CD']['delta']
+        self.lon['CX']['T'] = self.FC['CTs']*np.cos(self.FC['epss'])
+        self.lon['CZ']['u'] = -self.model['CT']['u']*np.sin(self.FC['epss']) - self.model['CL']['u']
+        self.lon['CZ']['alpha'] = -self.model['CT']['alpha']*np.sin(self.FC['epss']) -self.model['CL']['alpha'] - self.FC['CDs']
+        self.lon['CZ']['alphadot'] = -self.model['CL']['alpha']
+        self.lon['CZ']['q'] = -self.model['CL']['q']
+        self.lon['CZ']['delta'] = -self.model['CL']['delta']
+        self.lon['CZ']['T'] = -self.FC['CTs']*np.sin(self.FC['epss'])
+        self.lon['Cm'] = self.model['Cm']
+        self.lon['Cm']['u'] = self.model['Cm']['u'] + self.model['Cm']['Tu']
+
+                # - Transfer functions -
+        # Longitudinal
+        self.lon['G'] = lon_dynamics(self)
+        
+        # Lateral
+        self.lat['G'] = lat_dynamics(self)
