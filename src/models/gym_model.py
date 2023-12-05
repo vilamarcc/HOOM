87d1998236d33037ddf958c2c8674e9c11ed1d6a
Marc Vila <vilamarcc@gmail.com>
2023-12-05 13:00:31 +0100

    fix: gym_model updated, new implementation seems to work

--- src/models/gym_model.py
+++ src/models/gym_model.py
@@ -7,6 +7,7 @@ from gymnasium import spaces
 import gymnasium as gym
 
 import control as ctrl
+import control.matlab as ctrlm
 from src.models.plane_model import plane_model
 from src.utils.scales import angle_normalize
 from src.utils.conversion import to_time_domain
@@ -23,7 +24,7 @@ class ControlEnv(gym.Env):
 
         self.plane_model = plane_model
         self.ref_x = ref_x
-        self.state = np.array([0, 0, 0], dtype=np.float32)
+        self.state = np.array([0, 0, 0, 0], dtype=np.float32)
 
         self.A = self.plane_model.lon['A']
         self.B = self.plane_model.lon['B']
@@ -33,6 +34,8 @@ class ControlEnv(gym.Env):
         self.max_deltae = deltae_max
         self.max_deltaT = deltaT_max
 
+        self.sys = sys = ctrl.ss(self.A, self.B, np.eye(4), np.zeros((4, 2)), self.dt)
+
         self.xs, self.us = [], []
         self.ts = []
         self.ts.append(0)
@@ -40,7 +43,7 @@ class ControlEnv(gym.Env):
         self.render_mode = render_mode
 
         u_max = np.array([self.max_deltae, self.max_deltaT])
-        x_max = np.array([10., 1., 5.])
+        x_max = np.array([1., 1., 1., 1.])
 
         self.action_space = spaces.Box(
             low=-u_max, high=u_max, shape=(2,), dtype=np.float32
@@ -49,14 +52,15 @@ class ControlEnv(gym.Env):
         self.observation_space = spaces.Box(low=-x_max, high=x_max, dtype=np.float32)
 
     def step(self, action):
-        du, alpha, theta= self.state[0], self.state[1], self.state[2]
+        du, alpha, q, theta= self.state[0], self.state[1], self.state[2], self.state[3]
         deltae, deltaT = action
 
         dt = self.dt
 
         u = [deltae, deltaT]
-        x = np.array([du, alpha, theta], dtype=np.float32)
-        u = np.clip(u, -self.max_deltae, self.max_deltae)
+        x = np.array([du, alpha, q, theta], dtype=np.float32)
+        u[0] = np.clip(u[0], -self.max_deltae, self.max_deltae)
+        u[1] = np.clip(u[1], -self.max_deltaT, self.max_deltaT)
         t_n = self.ts[-1]
         
         self.last_u = u # for rendering
@@ -72,9 +76,8 @@ class ControlEnv(gym.Env):
             done = True
         else:
             done = False
-
-        sys = ctrl.StateSpace(to_time_domain(self.A).subs(t,t_n), to_time_domain(self.B).subs(t, t_n), np.eye(3), np.zeros((3, 2)), self.dt)
-        x_dot = sys.dynamics(t_n, x, u)
+        
+        x_dot = self.sys.dynamics(t_n, x, u)
         x_n = x + x_dot * dt
 
         if self.render_mode == "human":
@@ -90,14 +93,14 @@ class ControlEnv(gym.Env):
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
         if options is None:
-            x_max = np.array([10, 1, 5], dtype=np.float32)
+            x_max = np.array([10, 1, 1, 5], dtype=np.float32)
             self.xs, self.us = [], []
  
         return self._get_obs(), {}
     
     def _get_obs(self):
-        du, alpha, theta= self.state
-        return np.array([du, alpha, theta], dtype=np.float32)
+        du, alpha, q, theta= self.state
+        return np.array([du, alpha, q, theta], dtype=np.float32)
 
     def render(self):
         #TODO
